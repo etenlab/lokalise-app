8972c6eab93dbab2d0d784141cc00c0a40a13751
Michael Marshall <michaelmarshall220@gmail.com>
2023-01-03 12:45:29 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lokalise-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
